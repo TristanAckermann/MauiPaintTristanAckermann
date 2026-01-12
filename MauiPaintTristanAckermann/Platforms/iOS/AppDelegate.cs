69e7a5de12a85f8f821eb98b4245a3e0326ab812
tristan <tristan.ackermann@outlook.com>
2026-01-12 09:25:47 +0100

    initial commit

---
+++ MauiPaintTristanAckermann/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiPaintTristanAckermann;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
