73a76f6374d9553abd135b4dc982c882dd89826b
tristan <tristan.ackermann@outlook.com>
2026-01-13 16:52:10 +0100

    added db because login page was usles without

--- MauiPaintTristanAckermann/App.xaml.cs
+++ MauiPaintTristanAckermann/App.xaml.cs
@@ -14,7 +14,8 @@ public partial class App : Application
         if (!string.IsNullOrEmpty(storedUser))
         {
             // User exists -> Auto Login
-            drawingService.SetUser(storedUser);
+            // Fire and forget (Gallery will populate when ready)
+            _ = drawingService.SetUser(storedUser);
             MainPage = new AppShell();
         }
         else
